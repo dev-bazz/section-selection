9d12105254f604af895fbf429bce5f19674d50cf
BaZz <bazuaye50@gmail.com>
2023-07-29 19:03:18 +0100

    init files

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	/**
+	 * @description: Application Routing System
+	 * @documentation: https://reactrouter.com/en/6.14.2/start/overview
+	 */
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+//  Render Application
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
